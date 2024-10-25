b2a332e73ba29da0fda0b47cdebdfcfe6da443f0
TA Trikalinos <ttrikalin@mac.com>
2024-10-25 10:27:06 -0400

    finished section 2

--- materials/figures/Section2_point_process.R
+++ materials/figures/Section2_point_process.R
@@ -75,7 +75,6 @@ dat <- data.frame(times = dat)
 #############################
 p2 <- ggplot() +
   xlim(0, 5) + 
-  ylim(0, 0.5) +
   geom_segment(aes(x = 1, xend = 5, y = 0, yend = 0), color = "black", linewidth = 2) +
   theme_bw() + 
   xlab("time") +
@@ -92,7 +91,7 @@ p2 <- ggplot() +
   theme(panel.border= element_blank())+
   theme(axis.line.x = element_line(color="black")) 
 
-p2 + 
+p2 + ylim(0, 0.5) +
   geom_histogram(aes(x = dat$times[1:100], y=..density..), 
                  color = "black", 
                  fill = "gray", 
@@ -101,7 +100,7 @@ p2 +
 if(T) ggsave("05_point_process.pdf", width = 5, height = 3)
 
 
-p2 + 
+p2 + ylim(0, 0.5) +
   geom_histogram(aes(x = dat$times[1:1000], y=..density..), 
                  color = "black", 
                  fill = "gray", 
@@ -109,7 +108,7 @@ p2 +
                  center=0.025)
 if(T) ggsave("06_point_process.pdf", width = 5, height = 3)
 
-p2 + 
+p2 + ylim(0, 0.5) +
   geom_histogram(aes(x = dat$times[1:10000], y=..density..), 
                  color = "black", 
                  fill = "gray", 
@@ -117,7 +116,7 @@ p2 +
                  center=0.025)
 if(T) ggsave("07_point_process.pdf", width = 5, height = 3)
 
-p2 + 
+p2 + ylim(0, 0.5) +
   geom_histogram(aes(x = dat$times[1:10000], y=..density..), 
                  color = "black", 
                  fill = "gray", 
@@ -130,11 +129,101 @@ if(T) ggsave("08_point_process.pdf", width = 5, height = 3)
 
 
 
-p2 + 
-  geom_histogram(aes(x = first_times[,1], y=..density..), 
+
+p2 + ylim(0, 1) +
+  geom_histogram(aes(x = dat$times[1:10000], y=..density..), 
+                 color = "black", 
+                 fill = "gray", 
+                 binwidth = 0.05, 
+                 center=0.025) + 
+  geom_segment(aes(x = 1, xend = 5, y = 1/4, yend = 1/4), color = "red", linewidth = 1) + 
+  geom_segment(aes(x = 0, xend = 1, y = 0, yend = 0), color = "red", linewidth = 1)
+
+if(T) ggsave("09_point_process.pdf", width = 5, height = 3)
+
+
+p2 + ylim(0, 1) + 
+  geom_histogram(aes(x = first_times[,1], y=after_stat(density)), 
                  color = "black", 
                  fill = "gray", 
                  binwidth = 0.05, 
                  center=0.025) 
 
-if(T) ggsave("08_point_process.pdf", width = 5, height = 3)
+if(T) ggsave("10_point_process.pdf", width = 5, height = 3)
+
+##################### loglinear ############################
+
+L <- function(t, alpha = -1, beta = 0.5, ...) {
+  (exp(alpha + beta * t) - exp(alpha)) / beta
+}
+
+## ----Lambda-inv---------------------------------------------------------------
+Li <- function(z, alpha = -1, beta = 0.5, ...) {
+  (log(beta * z + exp(alpha)) - alpha) / beta
+}
+
+
+set.seed(20241011)
+Z <- nhppp::vdraw_cumulative_intensity(
+    Lambda = L,
+    Lambda_inv = Li,
+    t_min = rep(1, 10^5),
+    t_max = 5,
+    atmost1 = FALSE
+  )
+dat <- data.table(Z)
+
+p2 + 
+  geom_histogram(aes(x = dat$V1, y = after_stat(density)), 
+                 color = "black", 
+                 fill = "gray", 
+                 binwidth = 0.05, 
+                 center=0.025) 
+
+if(T) ggsave("12_point_process.pdf", width = 5, height = 3)
+
+dat_long <- melt(data = dat, value.name = "V", measure.vars = 1:ncol(Z))
+dat_long <- dat_long[!is.na(V),]
+
+p2 + 
+  geom_histogram(aes(x = dat_long$V, y = after_stat(density)), 
+                 color = "black", 
+                 fill = "gray", 
+                 binwidth = 0.05, 
+                 center=0.025) 
+
+if(T) ggsave("11_point_process.pdf", width = 5, height = 3)
+
+
+########################### lambda and Lambda 
+
+
+p3 <- ggplot() + 
+  xlim(0, 5) + 
+  theme_bw() + 
+  xlab("time") +
+  theme(axis.text.x = element_text(size = 14),       
+        axis.title.x = element_text(size = 18))  +  
+  theme(axis.title.y = element_text(size = 18), 
+        axis.text.y = element_text(size = 14)#, 
+        #axis.ticks.y = element_blank()
+  ) + 
+  theme(plot.background = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank() )+
+  theme(panel.border= element_blank())+
+  theme(axis.line.x = element_line(color="black")) 
+
+p3 + ylim(0, 5) + ylab("intensity") +
+  geom_segment(aes(x = 0, xend = 1, y =0, yend = 0), color = "red", linewidth = 1) + 
+  geom_segment(aes(x = 1, xend = 5, y =1, yend = 1), color = "red", linewidth = 1)
+if(T) ggsave("13_point_process.pdf", width = 5, height = 3)
+
+
+p3 + ylim(0, 5) + ylab("cumulative intensity") +
+  geom_segment(aes(x = 0, xend = 1, y =0, yend = 0), color = "red", linewidth = 1) + 
+  stat_function(fun = function(x) x - 1, color = "red", linewidth = 1)
+if(T) ggsave("14_point_process.pdf", width = 5, height = 3)
+
+
+
