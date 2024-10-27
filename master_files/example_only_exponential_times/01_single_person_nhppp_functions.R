ec0d63137bd4550f39b76676f6d3ce500a1277ba
TA Trikalinos <ttrikalin@mac.com>
2024-10-26 22:09:13 -0400

    one person at a time

--- master_files/example_only_exponential_times/01_single_person_nhppp_functions.R
+++ master_files/example_only_exponential_times/01_single_person_nhppp_functions.R
@@ -1,9 +1,12 @@
 #### The nhppp package syntax for a single person
 library(nhppp)
+library(ggplot2)
+
 
 # single person
 # constant intensity = 1/70 
 # in the interval 20 to 110
+baseplot <- ggplot() + xlim(0, 120) + xlab("time") 
 
 ########################################################
 # Constant intensity 
@@ -11,12 +14,10 @@ library(nhppp)
 
 # note the vectorized lambda
 l_constant <- function(t, lambda = 1/70, ... ) t*0 + lambda
-curve(l_constant, 
-      from = 20, 
-      to = 110, 
-      ylab = "intensity", 
-      xlab = "time", 
-      main = "Constant intensity = 1/70")
+
+
+baseplot + ylim(0, 0.02) + ylab("intensity") + ggtitle("Constant") +
+  stat_function(fun = l_constant, col = "red", xlim = c(20, 110))
 
 
 # red process: 0, or 1 events 
@@ -53,20 +54,21 @@ l_weibull <- function(t, shape = 7, scale = 150, ...) {
   shape / scale * (t/scale)^(shape - 1)
 }
 
+baseplot + ylim(0, 0.01) + ylab("intensity") +  ggtitle("Weibull") + 
+  stat_function(fun = l_weibull, col = "red", xlim = c(20, 110))
+
 L_weibull <- function(t, shape = 7, scale = 150, ...) {
    (t/scale)^shape
 }
+
+baseplot + ylim(0, 0.15) + ylab("cumulative intensity") +  ggtitle("Weibull") + 
+  stat_function(fun = L_weibull, col = "red", xlim = c(20, 110))
 
 Li_weibull <- function(z, shape = 7, scale = 150, ... ) {
   scale * z^(1/shape)
 }
 
-curve(l_weibull, 
-      from = 20, 
-      to = 110, 
-      ylab = "intensity", 
-      xlab = "time", 
-      main = "Weibull intensity, scale = 150, shape = 7")
+
 
 # as above, using the intensity (l_*) and a constant linear majorizer
 # black process: exactly 1 event
@@ -100,12 +102,8 @@ l_beat <- function(t, max_exposure = 1, start_age = 20, stop_age = 110, ...) {
 # We can get a cumulative intensity (L_*), but not a cheap inverse. 
 # So lets focus on intensity sampling. 
 # We need to do some work to find a majorizer
-curve(l_beat, 
-      from = 20, 
-      to = 110, 
-      ylab = "intensity", 
-      xlab = "time", 
-      main = "Constant intensity = 1/70")
+baseplot + ylim(0, 1) + ylab("intensity") +  ggtitle("Beat") + 
+  stat_function(fun = l_beat, col = "red", xlim = c(20, 110), n=300)
 
 
 # black process: exactly 1 event 
--- master_files/example_only_exponential_times/02_find_a_majorizer.R
+++ master_files/example_only_exponential_times/02_find_a_majorizer.R
@@ -1,7 +1,17 @@
+library(nhppp)
+library(ggplot2)
+
+
+baseplot <- ggplot() + xlim(0, 120) + xlab("time") 
+
 # Weibull intensity 
 l_weibull <- function(t, shape = 7, scale = 150, ...) {
   shape / scale * (t/scale)^(shape - 1)
 }
+weibull_plot <- 
+  baseplot + ylim(0, 0.01) + ylab("intensity") +  ggtitle("Weibull") + 
+    stat_function(fun = l_weibull, col = "red", xlim = c(20, 110))
+weibull_plot
 
 # for monotone functions, the maximum is at a bound
 # -- lower bound for decreasing 
@@ -11,6 +21,13 @@ lower_bounds <- l_weibull(t = 20)
 upper_bounds <- l_weibull(t = 110)
 majorizer_intercept <- 
   pmax(lower_bounds, upper_bounds)  # `max()` would do too.
+
+weibull_plot + 
+  geom_segment(aes(x = 20, 
+                   xend = 110, 
+                   y = majorizer_intercept, 
+                   yend = majorizer_intercept), 
+               col = "blue", linewidth = 1)
 
 # for the interval from 20 to 110
 nhppp::draw(
@@ -27,12 +44,18 @@ time_breaks <- seq(from = 20, to = 110, length.out = 5+1)
 lower_bounds <- l_weibull(time_breaks[1:5])
 upper_bounds <- l_weibull(time_breaks[2:6])
 
-l_star_vector <- 
+l_star_weibull <- 
   pmax(lower_bounds, upper_bounds) # `max()` won't do
+
+weibull_plot + 
+  geom_step(data = data.frame(x = time_breaks, y=l_star_weibull[c(1, 1:5)]),
+            aes(x, y), 
+            direction = "vh", col = "blue", linewidth = 1)
+
 
 nhppp::draw(
   lambda = l_weibull, 
-  step_majorizer_vector = l_star_vector,
+  step_majorizer_vector = l_star_weibull,
   t_min = 20, 
   t_max = 110, 
   atmost1 = TRUE, 
@@ -55,11 +78,40 @@ l_beat <- function(t, max_exposure = 1, start_age = 20, stop_age = 110, ...) {
     (0.5 + (cos( (t+50) /2) + cos(0.9* (t+50) /2))/4)
 }
 
+beat_plot <- baseplot + ylim(0, 1.5) + ylab("intensity") +  ggtitle("Beat") + 
+  stat_function(fun = l_beat, col = "red", xlim = c(20, 110), n=300)
+beat_plot
+
 # This is a Lipschitz function. 
 # If you can find a bound for how fast if grows (cone coefficient)
 # you can always find an upper bound over the interval
 # max(upper_bound, lower_bound) + K * (t_max - t_min) / 2
 # The amplitude of the derivative of l_beat over time is 
 # a bound for K -- the rest is sin() functions (always <=1)
+# Here  K < 1/8 + .9/8 <2/8 = 1/4 is a very quick bound
+
+time_breaks <- seq(20, 110, length.out = 50)
+
+l_star_beat <- 
+  get_step_majorizer(
+    fun = l_beat,
+    breaks = time_breaks, 
+    is_monotone = FALSE, 
+    K = 1/4
+  )
+beat_plot + 
+  geom_step(data = data.frame(x = time_breaks, y=l_star_beat[c(1,1:49)]),
+            aes(x, y), 
+            direction = "vh", col = "blue", linewidth = 1)
+
+
+nhppp::draw(
+  lambda = l_beat, 
+  step_majorizer_vector = l_star_beat,
+  t_min = 20, 
+  t_max = 110, 
+  atmost1 = FALSE, 
+  atleast1 = FALSE)   
+
 
 
